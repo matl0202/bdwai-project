262ea1f301e1b72636973f714a3839699796042f
matl0202 <mat.l@vp.pl>
2025-01-14 23:19:36 +0100

    startowe

---
+++ DrumkitStore/DrumkitStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DrumkitStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
